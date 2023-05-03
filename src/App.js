594df77621e50fbb828d344d4197f0bccda1c991
Felix <felix@fuchs-zell.de>
2023-05-03 18:34:00 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [img, setImg] = useState('')
+  const [previewImg, setpreviewImg] = useState('')
+  const imgHandler = (event) => {
+  console.log(event.target.files)
+    setImg(event.target.files[0])
+    setpreviewImg(URL.createObjectURL(event.target.files[0]))
+  }
+   const checkingHandler = () => {
+   const formData = new FormData()
+    formData.append('img', img)
+
+    axios.post("http://localhost:8000/upload/", formData, {headers: {'content-type': img.type}}).then((res) => console.log(res))
+   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" className="form-control" name="upload_file" onChange={imgHandler}/>
+      <div className="container">
+      <div className="imgBox">
+      <img src={previewImg}/>
+      </div>
+      </div>
+      <button onClick={checkingHandler}>Check Image for Alpakas</button>
     </div>
   );
 }
